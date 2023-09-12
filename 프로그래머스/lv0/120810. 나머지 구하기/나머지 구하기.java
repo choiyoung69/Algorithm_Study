524ca076d0dfb3d1052357ce73bbf453f7869096
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2023-09-12 09:57:31 +0900

    Update 나머지 구하기.java
    여러 풀이를 적음

--- 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
@@ -5,3 +5,22 @@ class Solution {
         return answer;
     }
 }
+
+class Solution1 {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num%num2;
+        return anawer;
+    }
+}
+
+class Solution2 {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        while(num1 >= num2){
+            num1 = num1 - num2;
+        }
+        answer = num1;
+        return anawer;
+    }
+}
