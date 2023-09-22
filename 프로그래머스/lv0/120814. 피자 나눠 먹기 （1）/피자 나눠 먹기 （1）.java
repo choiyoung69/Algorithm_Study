bbd0d16541c4f5e7c952ff56bd38e7e40b348df9
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2023-09-22 13:49:54 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(answer < (double)n/7){
+            answer += 1;
+        }
+        return answer;
+    }
+}
