f9032637030864fdd2487d509a73ae5af1992cc9
choiyoung69 <choiyoung609@gmail.com>
2024-03-19 19:34:23 +0900

    12번문제

--- 인강_문제/1.문자열/5. 특정문자 뒤집기/Main.java
+++ 인강_문제/1.문자열/5. 특정문자 뒤집기/Main.java
@@ -1,24 +1,36 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args){
-		Scanner in=new Scanner(System.in);
-		String input = in.nextLine();
-
-		String output = "";
-
-		for(int i = 0; i < input.length(); i++){
-			int check = 0;
-			for(int j = 0; j < output.length(); j++){
-				if(input.charAt(i) == output.charAt(j)){
-					check = 1;
-					break;
-				}
-			}
-			if(check == 0){
-				output += input.charAt(i);
-			}
-		}
-		System.out.print(output);
-	}
+public class Main{
+  public static void main(String[] args){
+    	String temp = "";
+   
+    	Scanner sc = new Scanner(System.in);
+    	String input = sc.nextLine();
+    	
+    	// 문자열 확인 후 문자 뒤집기
+    	for(int i = 0; i < input.length(); i++){
+    		if(Character.isAlphabetic(input.charAt(i))){
+    			temp += input.charAt(i);
+    		}
+    	}
+    	String alpha = "";
+    	for(int i = 0; i < temp.length(); i++){
+    		alpha += temp.charAt(temp.length() - 1 - i);	
+    	}
+    	
+    	//문자열 자리에 뒤집은 문자열 넣기
+        String output = "";
+        int count = 0;
+        for(int i = 0; i < input.length(); i++){
+        	if(Character.isAlphabetic(input.charAt(i))){
+            	output += alpha.charAt(count);
+            	count++;
+            }
+            else{
+                output += input.charAt(i);
+            }
+        }
+               
+        System.out.println(output);
+  	}
 }
