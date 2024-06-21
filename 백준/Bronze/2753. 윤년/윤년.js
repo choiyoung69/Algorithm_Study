32d44d0c2ba758ce0eda8585ed87b394fbeff23f
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2024-06-21 22:52:32 +0900

    [Bronze V] Title: 윤년, Time: 160 ms, Memory: 9488 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+rl.on("line", function(line){
+    let input = Number(line);
+    if(((input%4 === 0)&& (input%100 !== 0))||(input%400 === 0)){
+        console.log(1);
+    }
+    else console.log(0);
+})
