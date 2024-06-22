651b3318b8ab2c0aaec4a9a329cd4446622e9bbd
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2024-06-22 10:31:22 +0900

    [Bronze V] Title: A+B - 7, Time: 148 ms, Memory: 9540 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function(line){
+    input.push(line.split(' ').map((e1) => Number(e1)));
+}).on("close", function(){
+    let N = input[0][0];
+    let result = "";
+    for(let i = 1; i < N+1; i++){
+        result += "Case #" + i + ": " + (input[i][0] + input[i][1]) + "\n";
+    }
+    console.log(result);
+});
