1fa17592a42baf3139af94e1b7d0a53926acb153
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2024-06-28 10:51:21 +0900

    [Bronze II] Title: 상수, Time: 144 ms, Memory: 9468 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.js
+++ 백준/Bronze/2908. 상수/상수.js
@@ -5,16 +5,11 @@ const rl = readline.createInterface({
 });
 
 rl.on("line", function(line){
-    let arr = line.trim().split(' ')
-    let num1 = "";
-    let num2 = "";
-    for(let i = 0; i < arr[0].length; i++){
-        num1 += arr[0][arr[0].length - 1 - i];
+    let arr = line.split(' ');
+    
+    function strReverse(str){
+        return Number(str.split('').reverse().join(''))
     }
-    for(let i = 0; i < arr[1].length; i++){
-        num2 += arr[1][arr[1].length - 1 - i];
-    }
-    num1 = Number(num1);
-    num2 = Number(num2);
-    console.log(Math.max(num1, num2));
+    
+    console.log(Math.max(strReverse(arr[0]), strReverse(arr[1])))
 })
