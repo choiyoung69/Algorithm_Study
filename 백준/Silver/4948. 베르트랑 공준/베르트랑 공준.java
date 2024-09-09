96fb568079740609613ef51630486d5ec4199abb
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2024-09-09 09:29:26 +0900

    [Silver II] Title: 베르트랑 공준, Time: 144 ms, Memory: 14720 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -5,42 +5,37 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Main{
+    public static boolean[] arrList = new boolean[246913];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        int N = Integer.parseInt(br.readLine());
-        while(N != 0){
-            arr.add(N);
+        get_prime();
+
+        int N = 0;
+        while(true) {
             N = Integer.parseInt(br.readLine());
+            if(N == 0) break;
+            int count = 0;
+            for(int j = N+ 1; j <= N*2; j++){
+                    if(!arrList[j]) count++;
+            }
+            sb.append(count).append("\n");
         }
 
-        int max = Collections.max(arr);
+        System.out.println(sb);
+    }
 
-
-        boolean[] arrList = new boolean[2 * max + 1];
-
+    static public void get_prime(){
         arrList[0] = true;
         arrList[1] = true;
-
-        int end = max * 2;
+        int end = arrList.length;
         for(int i = 2; i <= Math.sqrt(end); i++){
             if(arrList[i]) continue;
             for (int j = i * i; j <= end; j += i) {
                 arrList[j] = true;
             }
         }
-
-        int count = 0;
-        for (int number : arr) {
-            count = 0;
-            for(int j = number+ 1; j <= number*2; j++){
-                if(!arrList[j]) count++;
-            }
-            sb.append(count).append("\n");
-        }
-
-        System.out.println(sb);
     }
 }
