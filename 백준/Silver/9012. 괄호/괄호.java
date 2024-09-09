4c477501f0431bbd06b46fb52160cc658721764d
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2024-09-09 10:55:01 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,48 +1,34 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Stack;
- 
-public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < T; i++) {
-			sb.append(solve(br.readLine())).append('\n');
-		}
-		
-		System.out.println(sb);
-	}
- 
-	public static String solve(String s) {
- 
-		Stack<Character> stack = new Stack<>();
- 
-		for (int i = 0; i < s.length(); i++) {
- 
-			char c = s.charAt(i);
- 
-			if (c == '(') {
-				stack.push(c);
-			}
-			else if (stack.empty()) {
-				return "NO";
-			}
-			else {
-				stack.pop();
-			}
-		}
- 
-		if (stack.empty()) {
-			return "YES";
-		} 
-		else {
-			return "NO";
-		}
-	}
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Main{
+    public static Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < N; i++){
+            String str = br.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(j);
+                if(ch == '(') stack.push('(');
+                else if(ch == ')' && stack.isEmpty()) {
+                    stack.push(')');
+                    break;
+                }
+                else if(ch == ')') stack.pop();
+            }
+            if(!stack.isEmpty()) sb.append("NO").append('\n');
+            else sb.append("YES").append('\n');
+            stack.clear();
+        }
+        System.out.println(sb);
+    }
 }
