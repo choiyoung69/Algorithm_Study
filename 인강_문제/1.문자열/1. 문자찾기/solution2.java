b382c85b224a305b98123d15a0c0c811d738dbd2
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2024-10-14 22:33:50 +0900

    Create solution2.java

---
+++ 인강_문제/1.문자열/1. 문자찾기/solution2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int solution(String str, char t){
+        int answer = 0;
+
+        String upper_str = str.toUpperCase();
+        char upper_t = Character.toUpperCase(t);
+        
+        for(int i = 0; i < upper_str.length(); i++){
+            if(upper_str.charAt(i) == upper_t) answer++;
+        }
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        char t = br.readLine().charAt(0);
+
+        System.out.println(Main.solution(str, t));
+    }
+}
