cdb8326292ad5786a93e20f0814418aea5527a9d
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2025-02-20 01:06:18 +0900

    Create main.java

---
+++ 인강_문제/5. Stack, Queue/1. 올바른 괄호/main.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public String solution(String s) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if(ch == '(') stack.push(ch);
+            else if(stack.isEmpty() || stack.pop() != '('){
+                return "NO";
+            }
+        }
+
+        if(!stack.isEmpty()) return "NO";
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        System.out.println(main.solution(s));
+    }
+}
