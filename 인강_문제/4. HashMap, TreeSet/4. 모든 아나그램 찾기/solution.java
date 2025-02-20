fbc0f5196959d762d26cd0e4e9c13ecd31b5de72
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2025-02-20 18:57:49 +0900

    Update solution.java

--- 인강_문제/4. HashMap, TreeSet/4. 모든 아나그램 찾기/solution.java
+++ 인강_문제/4. HashMap, TreeSet/4. 모든 아나그램 찾기/solution.java
@@ -4,27 +4,21 @@ import java.util.*;
 public class Main {
     public int solution(String S, String K) {
         int answer = 0;
-        Map<Character, Integer> m = new HashMap<>();
-        Map<Character, Integer> k = new HashMap<>();
-        int lt = 0, rt = 0;
+        Map<Character, Integer> am = new HashMap<>();
+        Map<Character, Integer> bm = new HashMap<>();
 
-        for(int i = 0; i < K.length(); i++){
-            k.put(K.charAt(i), k.getOrDefault(K.charAt(i), 0) + 1);
+        for(char x: K.toCharArray()) bm.put(x, bm.getOrDefault(x, 0) + 1);
+        for(int i = 0; i < K.length() - 1; i++){
+            am.put(S.charAt(i), am.getOrDefault(S.charAt(i), 0) + 1);
         }
 
-        for (rt = 0; rt < S.length(); rt++) {
-            if(rt - lt + 1 == K.length()) {
-                m.put(S.charAt(rt), m.getOrDefault(S.charAt(rt), 0) + 1);
-                if(m.equals(k)) {
-                    answer++;
-                }
-                if(m.get(S.charAt(lt)) == 1) m.remove(S.charAt(lt));
-                else m.put(S.charAt(lt), m.get(S.charAt(lt)) - 1);
-                lt++;
-            }
-            else {
-                m.put(S.charAt(rt), m.getOrDefault(S.charAt(rt), 0) + 1);
-            }
+        int lt = 0;
+        for(int rt = K.length() - 1; rt < S.length(); rt++){
+            am.put(S.charAt(rt), am.getOrDefault(S.charAt(rt), 0) + 1);
+            if(am.equals(bm)) answer++;
+            am.put(S.charAt(lt), am.getOrDefault(S.charAt(lt), 0) - 1);
+            if(am.get(S.charAt(lt)) == 0) am.remove(S.charAt(lt));
+            lt++;
         }
 
         return answer;
