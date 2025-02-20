0f8ab9cfc824a93e99140a2aec2d9efa372926f2
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2025-02-21 01:53:51 +0900

    Create main.java

---
+++ 인강_문제/6. Sorting and Searching/2. 버블정렬/main.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public int[] solution(int N, int[] arr) {
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        for (int x : main.solution(N, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
