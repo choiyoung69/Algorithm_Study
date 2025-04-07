189cadb432031c7384cd6c9a9e65a76fd7fc8990
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2025-04-07 16:50:32 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18096 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -5,18 +5,24 @@ class Main {
 
     public int solution(int N) {
         int[] arr = new int[N + 1];
-        arr[0] = 0;
-        arr[1] = 0;
+        arr[0] = arr[1] = 0;
+        if(N <= 1) return arr[N];
 
-        for(int i = 2; i <= N; i++) {
-            arr[i] = arr[i - 1] + 1;
-
-            if(i % 3 == 0) {
-                arr[i] = Math.min(arr[i], arr[i/3] + 1);
+        int i = 2;
+        while(arr[N] == 0) {
+            if(i % 6 == 0) {
+                arr[i] = Math.min(arr[i - 1], Math.min(arr[i / 3], arr[i / 2])) + 1;
             }
-            if(i % 2 == 0) {
-                arr[i] = Math.min(arr[i], arr[i/2] + 1);
+            else if(i % 3 == 0) {
+                arr[i] = Math.min(arr[i - 1], arr[i / 3]) + 1;
             }
+            else if(i % 2 == 0){
+                arr[i] = Math.min(arr[i - 1], arr[i / 2]) + 1;
+            }
+            else {
+                arr[i] = arr[i - 1] + 1;
+            }
+            i++;
         }
 
         return arr[N];
