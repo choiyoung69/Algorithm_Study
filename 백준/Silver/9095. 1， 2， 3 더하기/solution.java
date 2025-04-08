e97258312a4f879773d52afc9bd5a23bdd09a023
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2025-04-08 18:40:34 +0900

    Create solution.java

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/solution.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Arrays;
+
+class Main {
+    public int[] solution(int N, int[] arr) {
+        int[] answer = new int[N];
+        int max = Arrays.stream(arr).max().getAsInt();
+        int[] addResult = new int[max + 1];
+
+        addResult[0] = 1;
+        addResult[1] = 1;
+        addResult[2] = 2;
+
+        if(max > 2) {
+            for (int i = 3; i <= max; i++) {
+                addResult[i] = addResult[i - 3] + addResult[i - 2] + addResult[i - 1];
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            answer[i] = addResult[arr[i]];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException{
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int x : main.solution(N, arr)) {
+            System.out.println(x);
+        }
+    }
+}
