d1d52d2c6123a1f55bdc49f1ca83aa28b8bb4d50
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2025-09-12 02:15:28 +0900

    [Silver V] Title: 영화감독 숌, Time: 256 ms, Memory: 85592 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,21 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Main{
-    public static void main(String[] args) throws IOException {
+class Main
+{
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        int count = 1;
-        int num = 666;
-
-        while(count != N){;
-            num++;
-            if(String.valueOf(num).contains("666")) count++;
+        int i = 666;
+        while (N != 0) {
+            if (Integer.toString(i).contains("666")) {
+                N--;
+            }
+            i++;
         }
-
-        System.out.println(num);
+        System.out.println(i - 1);
     }
 }
