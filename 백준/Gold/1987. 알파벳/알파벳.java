c8a5ce3fd97715e257cfb00f502fb7ba55367311
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2026-03-20 16:50:45 +0900

    [Gold IV] Title: 알파벳, Time: 844 ms, Memory: 14800 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -4,48 +4,48 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int[] dx = {1, 0, -1, 0};
-    private static int[] dy = {0, 1, 0, -1};
+    private static int count = 0;
+    private static int[] dx = {0, 1, 0, -1};
+    private static int[] dy = {1, 0, -1, 0};
+    private static int R, C;
     private static char[][] arr;
-    private static boolean[] visited = new boolean[30];
-    private static int M;
-    private static int N;
-    private static int max = 1;
 
-    public static void dfs(int cy, int cx, int count) {
+    public static void dfs(int y, int x, int visited, int now) {
+        count = Math.max(count, now);
+
+        visited |= (1 << (arr[y][x] - 'A')); //현재 비트 켜기
+
         for (int i = 0; i < 4; i++) {
-            int nx = cx + dx[i];
-            int ny = cy + dy[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-            if(nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
-            if(visited[arr[ny][nx] - 'A']) continue;
+            if (nx < 0 || ny < 0 || nx >= C || ny >= R) continue;
 
-            max = Math.max(max, count + 1);
+            // 이미 방문한 곳이면 방문 X
+            int bit = 1 << (arr[ny][nx] - 'A');
+            if ((visited & bit) != 0) continue;
 
-            visited[arr[ny][nx] - 'A'] = true;
-            dfs(ny, nx, count + 1);
-            visited[arr[ny][nx] - 'A'] = false;
+            dfs(ny, nx, visited | bit, now + 1);
         }
     }
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new char[N][M];
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = str.charAt(j);
+        arr = new char[R][C];
+
+        for (int i = 0; i < R; i++) {
+            String temp = br.readLine();
+            for (int j = 0; j < C; j++) {
+                arr[i][j] = temp.charAt(j);
             }
         }
-
-        visited[arr[0][0] - 'A'] = true;
-        dfs(0, 0, 1);
-        System.out.println(max);
+        int startBit = 1 << (arr[0][0] - 'A');
+        dfs(0, 0, startBit, 1);
+        System.out.println(count);
     }
 }
