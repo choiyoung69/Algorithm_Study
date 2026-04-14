4a6e82ad6f69a60b9dae759cf36255c4811d3b33
choiyoung69 <122353155+choiyoung69@users.noreply.github.com>
2026-04-14 11:08:10 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15996 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,73 +1,66 @@
 import java.io.BufferedReader;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
-class Point {
-    int x;
-    int y;
+public class Main {
+    private static int N;
+    private static int M;
+    private static int[][] arr;
+    private static boolean[][] visited;
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
 
-    public Point(int y, int x) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class Main
-{
-    private static int T, N, M, K;
-    private static int[][] a;
-    private static int[][] visited;
-    private static int dy[] = {-1, 0, 1, 0};
-    private static int dx[] = {0, 1, 0, -1};
-    private static int worm = 0;
-
-    public static void go(int y, int x) {
-        visited[y][x] = 1;
+    public static void dfs(int y, int x) {
+        if(visited[y][x]) return;
+        visited[y][x] = true;
 
         for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
             int nx = x + dx[i];
+            int ny = y + dy[i];
 
-            if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-            if (visited[ny][nx] == 1) continue;
-            if (a[ny][nx] == 1) go(ny, nx);
+            if (nx < 0 || nx >= M || ny < 0 || ny >= N) {
+                continue;
+            }
+            if (arr[ny][nx] == 1 && !visited[ny][nx]) {
+                dfs(ny, nx);
+            }
         }
     }
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
 
-        int x, y;
-        for (int i = 0; i < T; i++) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-            a = new int[N][M];
-            visited = new int[N][M];
-            worm = 0;
-            for (int j = 0; j < K; j++) {
-                st = new StringTokenizer(br.readLine());
-                x = Integer.parseInt(st.nextToken());
-                y = Integer.parseInt(st.nextToken());
-                a[y][x] = 1;
-            }
-            find();
-            System.out.println(worm);
-        }
-    }
+            int K = Integer.parseInt(st.nextToken());
 
-    private static void find() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if(a[i][j] == 1 && visited[i][j] != 1){
-                    go(i, j);
-                    worm++;
+            arr = new int[N][M];
+            visited = new boolean[N][M];
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                arr[y][x] = 1;
+            }
+
+            int cnt = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (arr[i][j] == 1 && !visited[i][j]) {
+                        cnt++;
+                        dfs(i, j);
+                    }
                 }
             }
+            System.out.println(cnt);
+            T--;
         }
     }
 }
